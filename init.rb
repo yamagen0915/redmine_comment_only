df2bcbdcde414d26d933f7ba503133f80ea7a067
yamagen0915 <yamagen0915@gmail.com>
2018-02-02 12:58:47 +0900

    first commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :comment_only do
+  name 'Comment Only plugin'
+  author 'yamagen0915'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
